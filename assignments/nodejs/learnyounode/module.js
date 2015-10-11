c0f7c08e802c1b78c47dd6a0efee92574290a925
Benjamin Thompson <misterpink14@111s-Mac-mini.home>
2015-10-11 15:04:33 -0600

    finsihed learnyounode

---
+++ assignments/nodejs/learnyounode/module.js
@@ -0,0 +1,18 @@
+// MAKE IT MODULAR
+
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function (dir, filterStr, callback) {
+
+  fs.readdir(dir, function (err, list) {
+    if (err)
+      return callback(err)
+
+    list = list.filter(function (file) {
+      return path.extname(file) === '.' + filterStr
+    })
+
+    callback(null, list)
+  })
+}
---
+++ assignments/nodejs/learnyounode/program.js
@@ -0,0 +1,217 @@
+
+
+
+// HTTP JSON API SERVER
+var http = require('http')
+var url = require('url')
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime : time.getTime() }
+}
+
+var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url, true)
+  var time = new Date(parsedUrl.query.iso)
+  var result
+
+  if (/^\/api\/parsetime/.test(req.url))
+    result = parsetime(time)
+  else if (/^\/api\/unixtime/.test(req.url))
+    result = unixtime(time)
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(result))
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
+})
+server.listen(Number(process.argv[2]))
+
+
+// HTTP UPPERCASERER
+/*
+var http = require('http')
+var map = require('through2-map')
+
+var server = http.createServer(function (req, res) {
+  if (req.method != 'POST')
+    return res.end('send me a POST\n')
+
+  req.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
+*/
+
+// HTTP FILE SERVER
+/*
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+
+  fs.createReadStream(process.argv[3]).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
+*/
+
+// TIME SERVER - tcp
+/*
+var net = require('net')
+
+function zeroFill(i) {
+  return (i < 10 ? '0' : '') + i
+}
+
+function now () {
+  var d = new Date()
+  return d.getFullYear() + '-'
+    + zeroFill(d.getMonth() + 1) + '-'
+    + zeroFill(d.getDate()) + ' '
+    + zeroFill(d.getHours()) + ':'
+    + zeroFill(d.getMinutes())
+}
+
+var server = net.createServer(function (socket) {
+  socket.end(now() + '\n')
+})
+
+server.listen(Number(process.argv[2]))
+*/
+
+// JUGGLING ASYNC
+/*
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+function printResults () {
+  for (var i = 0; i < 3; i++)
+    console.log(results[i])
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err)
+
+      results[index] = data.toString()
+      count++
+
+      if (count == 3)
+        printResults()
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i)
+*/
+
+// HTTP COLLECT
+/*
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err)
+      return console.error(err)
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))  
+})
+*/ 
+
+// HTTP CLIENT
+/*
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+})
+*/
+
+// MAKE IT MODULAR
+/*
+var filter = require('./module.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filter(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+}) 
+*/
+
+// FILTERED LS
+/*
+var path = require('path')
+var fs = require('fs')
+var dir = process.argv[2]
+var ext = '.' + process.argv[3]
+
+fs.readdir(dir, function(err, files){
+	var filtered = files.filter( function(file) {
+		return path.extname(file) == ext
+	})
+	filtered.forEach( function(file) {
+		console.log(file)
+	})
+})
+*/
+
+// MY FIRST ASYNC I/O
+/*
+var fs = require('fs')
+var file = process.argv[2]
+
+fs.readFile(file, function (err, data){
+	console.log(data.toString().split('\n').length-1)
+})
+*/
+
+// MY FIRST I/O
+/*
+var fs = require('fs')
+var file = process.argv[2]
+
+var str = fs.readFileSync(file).toString()
+
+var  output = str.split('\n').length -1
+*/
+
+
+// BABY STEPS
+/*
+var output = 0
+
+for (var i = 2; i < process.argv.length; i++){
+	output += parseInt(process.argv[i])
+}
+*/
+
+// console.log(output)
